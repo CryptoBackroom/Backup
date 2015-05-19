a45e713e3a3af6bea84b9a36f299ef077cf82d83
CryptoBackroom <CryptoBackroom@gmail.com>
2015-05-19 18:28:33 -0400

    Revert "Change Default % reward"
    This reverts commit 749c45ac462440bacedb9f71dfdc9bd235affdd2.

--- src/main.h
+++ src/main.h
@@ -40,7 +40,7 @@ static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE =  100000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 1000000000 * COIN;
-static const int64_t COIN_YEAR_REWARD = 1 * CENT; // 1%
+static const int64_t COIN_YEAR_REWARD = 9 * CENT; // 9% after block 21000
 
 static const int MODIFIER_INTERVAL_SWITCH = 2700;
 
