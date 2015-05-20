a14e8d2abc0cea874b4585e11fcbe78746f4b5ed
CryptoBackroom <Cryptobackroom@gmail.com>
2015-05-20 02:54:05 -0400

    Seed Node

--- src/net.cpp
+++ src/net.cpp
@@ -1232,7 +1232,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"45.55.208.212", "45.55.208.212"},
+    {"104.236.29.198", "104.236.29.198"},
 
 };
 
