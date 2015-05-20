2f95800053e545796f54e7106a9740ab93a408fe
CryptoBackroom <Cryptobackroom@gmail.com>
2015-05-19 23:47:40 -0400

    Fix Premine

--- src/main.cpp
+++ src/main.cpp
@@ -985,7 +985,7 @@ int64_t GetProofOfWorkReward(int64_t nFees)
 {
     int64_t nSubsidy = 1 * COIN;
  
-    if(pindexBest->nHeight < 3)
+    if(pindexBest->nHeight < 2)
     {
         nSubsidy = 5 * COIN;
     }
